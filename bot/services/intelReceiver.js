92863425b5b014a2a19d15dd979adc6b9287900f
shadowjuice69 <shadowjuice69@gmail.com>
2026-07-28 03:28:29 -0500

    Add raw intel receiver debug

--- bot/services/intelReceiver.js
+++ bot/services/intelReceiver.js
@@ -12,7 +12,10 @@ function readBody(req) {
   return new Promise((resolve, reject) => {
     let body = "";
     req.on("data", chunk => body += chunk);
-    req.on("end", () => resolve(body));
+    req.on("end", () => {
+      console.log("[RAW INTEL RECEIVED]", body.substring(0, 300));
+      resolve(body);
+    });
     req.on("error", reject);
   });
 }
