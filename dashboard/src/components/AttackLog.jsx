3cbb593f8e73a1f3a910cd23967d77241b8eb711
shadowjuice69 <shadowjuice69@gmail.com>
2026-09-05 02:38:11 -0500

    Clean duplicate imports from dashboard repair workflow

--- dashboard/src/components/AttackLog.jsx
+++ dashboard/src/components/AttackLog.jsx
@@ -1,11 +1,6 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../services/supabase";
 import { loadNexusConfig, getNexusConfig } from "../services/nexusConfig";
-import { loadNexusConfig, getNexusConfig } from "../services/nexusConfig";
-import { loadNexusConfig, getNexusConfig } from "../services/nexusConfig";
-import { loadNexusConfig, getNexusConfig } from "../services/nexusConfig";
-import { loadNexusConfig, getNexusConfig } from "../services/nexusConfig";
-import { loadNexusConfig, getNexusConfig } from "../services/nexusConfig";
 
 const ATTACK_EMOJI = {
   attack: "⚔️",
